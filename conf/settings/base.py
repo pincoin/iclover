3a784a20a5363dd35e1fd81a46318b4219c5b7ea
pincoin <pincoins@gmail.com>
2019-02-13 10:22:49 +0900

    - base settings

---
+++ conf/settings/base.py
@@ -0,0 +1,81 @@
+import os
+
+from django.utils.translation import ugettext_lazy as _
+
+from . import BASE_DIR
+
+try:
+    from .secret import Secret
+except ImportError:
+    raise ImportError(
+        'Failed to import Secret values.'
+    )
+
+# Django apps
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# 3rd party packages
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'mptt',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+ROOT_URLCONF = 'conf.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'conf.wsgi.application'
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
+]
+
+# Internationalization
+LANGUAGE_CODE = 'ko-kr'
+LANGUAGES = [
+    ('ko', _('Korean')),
+    ('en', _('English')),
+]
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+TIME_ZONE = "Asia/Seoul"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
--- conf/settings/local.py
+++ conf/settings/local.py
@@ -1,15 +1,4 @@
-import os
-
-from django.utils.translation import ugettext_lazy as _
-
-from . import BASE_DIR
-
-try:
-    from .secret import Secret
-except ImportError:
-    raise ImportError(
-        'Failed to import Secret values.'
-    )
+from .base import *
 
 DEBUG = True
 
@@ -17,82 +6,6 @@ DEBUG = True
 SECRET_KEY = Secret.SECRET_KEY
 ALLOWED_HOSTS = Secret.ALLOWED_HOSTS
 DATABASES = Secret.DATABASES
-
-# Django apps
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-# 3rd party packages
-INSTALLED_APPS += [
-    'debug_toolbar',
-    'mptt',
-]
-
-# Our own apps
-INSTALLED_APPS += [
-    'core',
-    'member',
-    'shop',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-
-ROOT_URLCONF = 'conf.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'conf.wsgi.application'
-
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
-]
-
-# Internationalization
-LANGUAGE_CODE = 'ko-kr'
-LANGUAGES = [
-    ('ko', _('Korean')),
-    ('en', _('English')),
-]
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale'),
-)
-TIME_ZONE = "Asia/Seoul"
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/assets/'
@@ -103,3 +16,10 @@ STATICFILES_DIRS = [
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Our own apps
+INSTALLED_APPS += [
+    'core',
+    'member',
+    'shop',
+]
