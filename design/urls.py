7e4e3646f659a3678253d271d901a0c025355f55
youngminY <hihihixxx89@gmail.com>
2019-02-15 16:14:21 +0900

    - make designapp and recover extends error

--- design/urls.py
+++ design/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Product
 
 urlpatterns = [
     path('', Home.as_view()),
+    path('product/', Product.as_view()),
 ]
--- design/views.py
+++ design/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
-    template_name = 'design/base.html'
+    template_name = 'design/home.html'
+
+class Product(TemplateView):
+    template_name = 'design/Product.html'
