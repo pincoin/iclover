88ebcbf345ec91d80f15792506740f4e9dde110e
youngminY <hihihixxx89@gmail.com>
2019-02-15 16:29:46 +0900

    - make designapp and recover extends error

--- design/urls.py
+++ design/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Home, Product
 
 urlpatterns = [
-    path('', Home.as_view()),
-    path('product/', Product.as_view()),
+    path('', Home.as_view(), name='home'),
+    path('product/', Product.as_view(), name='product'),
 ]
