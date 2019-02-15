cd8750a26ea1d824e3a13604f69adc96103e0c0c
youngminY <hihihixxx89@gmail.com>
2019-02-15 17:41:22 +0900

    - delete shop app

--- shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import Home, Product
-
-urlpatterns = [
-    path('', Home.as_view()),
-    path('product/', Product.as_view()),
-]
--- shop/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class Home(TemplateView):
-    template_name = 'shop/home.html'
-
-class Product(TemplateView):
-    template_name = 'shop/product.html'
