15c45e02fe4ac276ba29a61532b9ec7dcbd6071c
youngminY <hihihixxx89@gmail.com>
2019-02-18 11:19:05 +0900

    - make checkout

--- design/urls.py
+++ design/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import Home, Product
-
+from .views import Home, Product, Cart
+from .views import Checkout,  Checkout2, Checkout3,  Checkout4, Checkout5
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('product/', Product.as_view(), name='product'),
+    path('cart/', Cart.as_view(), name='cart'),
+    path('cart/checkout/', Checkout.as_view(), name='checkout'),
+    path('cart/checkout2/', Checkout2.as_view(), name='checkout2'),
+    path('cart/checkout3/', Checkout3.as_view(), name='checkout3'),
+    path('cart/checkout4/', Checkout4.as_view(), name='checkout4'),
+    path('cart/checkout5/', Checkout5.as_view(), name='checkout5'),
 ]
--- design/views.py
+++ design/views.py
@@ -5,4 +5,22 @@ class Home(TemplateView):
     template_name = 'design/home.html'
 
 class Product(TemplateView):
-    template_name = 'design/Product.html'
+    template_name = 'design/product.html'
+
+class Cart(TemplateView):
+    template_name = 'design/cart.html'
+
+class Checkout(TemplateView):
+    template_name = 'design/checkout.html'
+
+class Checkout2(TemplateView):
+    template_name = 'design/checkout2.html'
+
+class Checkout3(TemplateView):
+    template_name = 'design/checkout3.html'
+
+class Checkout4(TemplateView):
+    template_name = 'design/checkout4.html'
+
+class Checkout5(TemplateView):
+    template_name = 'design/checkout5.html'
