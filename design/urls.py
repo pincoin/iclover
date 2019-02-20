462544b9c94b5e5c412303bf928486e9897b084d
youngminY <hihihixxx89@gmail.com>
2019-02-20 20:39:15 +0900

    - fixed url names

--- design/urls.py
+++ design/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import Home, Product, Cart, Checkout, Orders, Profile
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
-    path('product/', Product.as_view(), name='product'),
-    path('cart/', Cart.as_view(), name='cart'),
-    path('cart/checkout/', Checkout.as_view(), name='checkout'),
-    path('orders/', Orders.as_view(), name='orders'),
-    path('profile/', Profile.as_view(), name='profile'),
+    path('', Home.as_view(), name='design/home'),
+    path('product/', Product.as_view(), name='design/product'),
+    path('cart/', Cart.as_view(), name='design/cart'),
+    path('cart/checkout/', Checkout.as_view(), name='design/checkout'),
+    path('orders/', Orders.as_view(), name='design/orders'),
+    path('profile/', Profile.as_view(), name='design/profile'),
 ]
