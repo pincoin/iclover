ff94d012e52d9b4864ee110131a46fcf8740a249
youngminY <hihihixxx89@gmail.com>
2019-02-20 18:47:15 +0900

    - update admin pages

--- managing/urls.py
+++ managing/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Main
+from .views import Main, Customer, Product
 
 urlpatterns = [
     path('', Main.as_view(), name='main'),
+    path('customer', Customer.as_view(), name='customer'),
+    path('product', Product.as_view(), name='product'),
 
 ]
--- managing/views.py
+++ managing/views.py
@@ -3,3 +3,10 @@ from django.views.generic import TemplateView
 
 class Main(TemplateView):
     template_name = 'managing/main.html'
+
+class Customer(TemplateView):
+    template_name = 'managing/customer.html'
+
+class Product(TemplateView):
+    template_name = 'managing/product.html'
+
