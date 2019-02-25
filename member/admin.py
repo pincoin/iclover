5ea679e9314e158a73394e94f0e62142fd8fa1a7
pincoin <pincoins@gmail.com>
2019-02-25 21:35:41 +0900

    - profile model added

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Profile, LoginLog, PhoneVerificationLog
+)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class LoginLogAdmin(admin.ModelAdmin):
+    pass
+
+
+class PhoneVerificationLogAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(LoginLog, LoginLogAdmin)
+admin.site.register(PhoneVerificationLog, PhoneVerificationLogAdmin)
--- member/models.py
+++ member/models.py
@@ -1,3 +1,219 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
+from model_utils.models import TimeStampedModel
+
+
+class Profile(TimeStampedModel):
+    PHONE_VERIFIED_STATUS_CHOICES = Choices(
+        (0, 'unverified', _('cellphone unverified')),
+        (1, 'verified', _('cellphone verified')),
+        (2, 'revoked', _('cellphone revoked'))
+    )
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    phone = models.CharField(
+        verbose_name=_('phone number'),
+        max_length=16,
+        blank=True,
+        null=True,
+    )
+
+    address = models.CharField(
+        verbose_name=_('address'),
+        max_length=255,
+        blank=True,
+    )
+
+    phone_verified = models.BooleanField(
+        verbose_name=_('phone verified'),
+        default=False,
+    )
+
+    phone_verified_status = models.IntegerField(
+        verbose_name=_('cellphone verified status'),
+        choices=PHONE_VERIFIED_STATUS_CHOICES,
+        default=PHONE_VERIFIED_STATUS_CHOICES.unverified,
+        db_index=True,
+    )
+
+    date_of_birth = models.DateField(
+        verbose_name=_('date of birth'),
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
+
+    def __str__(self):
+        return '{} profile - user {}/{}'.format(self.id, self.user.id, self.user.username)
+
+    @property
+    def full_name(self):
+        # Only Hangul
+        pattern = re.compile(r'^[가-힣]+$')
+
+        if pattern.match(self.user.last_name) and pattern.match(self.user.first_name):
+            return '{}{}'.format(self.user.last_name, self.user.first_name)
+        else:
+            return '{} {}'.format(self.user.first_name, self.user.last_name)
+
+    full_name.fget.short_description = _('Full name')
+
+    @property
+    def email(self):
+        return self.user.email
+
+    email.fget.short_description = _('E-mail')
+
+
+class LoginLog(TimeStampedModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('user'),
+        null=True,
+        blank=True,
+        editable=True,
+        on_delete=models.SET_NULL,
+        related_name="%(app_label)s_%(class)s_owned",
+    )
+
+    ip_address = models.GenericIPAddressField(
+        verbose_name=_('IP address'),
+    )
+
+    class Meta:
+        verbose_name = _('login log')
+        verbose_name_plural = _('login logs')
+
+    def __str__(self):
+        return '{} {} {}'.format(self.user.email, self.ip_address, self.created)
+
+
+class PhoneVerificationLog(models.Model):
+    GENDER_CHOICES = Choices(
+        (0, 'female', _('female')),
+        (1, 'male', _('male')),
+    )
+
+    DOMESTIC_CHOICES = Choices(
+        (0, 'foreign', _('foreign')),
+        (1, 'domestic', _('domestic')),
+    )
+
+    token = models.CharField(
+        verbose_name=_('token'),
+        max_length=255,
+        blank=True,
+    )
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('user'),
+        null=True,
+        blank=True,
+        editable=True,
+        on_delete=models.SET_NULL,
+        related_name="phone_verification_logs",
+    )
+
+    code = models.CharField(
+        verbose_name=_('code'),
+        max_length=32,
+        blank=True,
+    )
+
+    reason = models.CharField(
+        verbose_name=_('reason'),
+        max_length=16,
+        blank=True,
+    )
+
+    result_code = models.CharField(
+        verbose_name=_('result code'),
+        max_length=16,
+        blank=True,
+    )
+
+    message = models.CharField(
+        verbose_name=_('message'),
+        max_length=255,
+        blank=True,
+    )
+
+    transaction_id = models.CharField(
+        verbose_name=_('transaction id'),
+        max_length=32,
+        blank=True,
+    )
+
+    di = models.CharField(
+        verbose_name=_('di'),
+        max_length=255,
+        blank=True,
+    )
+
+    ci = models.TextField(
+        verbose_name=_('ci'),
+        blank=True,
+    )
+
+    fullname = models.CharField(
+        verbose_name=_('fullname'),
+        max_length=32,
+        blank=True,
+    )
+
+    date_of_birth = models.CharField(
+        verbose_name=_('date of birth'),
+        max_length=16,
+        blank=True,
+    )
+
+    gender = models.IntegerField(
+        verbose_name=_('gender'),
+        choices=GENDER_CHOICES,
+        default=GENDER_CHOICES.male,
+        db_index=True,
+    )
+
+    domestic = models.IntegerField(
+        verbose_name=_('domestic'),
+        choices=DOMESTIC_CHOICES,
+        default=DOMESTIC_CHOICES.domestic,
+        db_index=True,
+    )
+
+    telecom = models.CharField(
+        verbose_name=_('telecom'),
+        max_length=16,
+        blank=True,
+    )
+
+    cellphone = models.CharField(
+        verbose_name=_('cellphone'),
+        max_length=32,
+        blank=True,
+    )
+
+    return_message = models.CharField(
+        verbose_name=_('return_message'),
+        max_length=255,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = _('phone verification log')
+        verbose_name_plural = _('phone verification logs')
+
+    def __str__(self):
+        return '{} {}'.format(self.fullname, self.cellphone)
