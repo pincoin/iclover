7b9b323753da5916b8b56dc8478b96905b05e2b8
younmin Yoon <hihihixxx89@gmail.com>
2019-12-02 14:27:03 +0900

    - connect aws

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -17,6 +17,51 @@ if DEBUG:
     # Media files (Uploaded files)
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+    # logging
+    LOGGING = {
+        'version': 1,
+        # 기존의 로깅 설정을 비활성화 할 것인가?
+        'disable_existing_loggers': False,
+
+        # 포맷터
+        # 로그 레코드는 최종적으로 텍스트로 표현됨
+        # 이 텍스트의 포맷 형식 정의
+        # 여러 포맷 정의 가능
+        'formatters': {
+            'format1': {
+                'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+                'datefmt': '%d/%b/%Y %H:%M:%S'
+            },
+            'format2': {
+                'format': '%(levelname)s %(message)s'
+            },
+        },
+
+        # 핸들러
+        # 로그 레코드로 무슨 작업을 할 것인지 정의
+        # 여러 핸들러 정의 가능
+        'handlers': {
+            # 콘솔(터미널)에 출력
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'format2',
+            }
+        },
+
+        # 로거
+        # 로그 레코드 저장소
+        # 로거를 이름별로 정의
+        'loggers': {
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+            },
+        },
+
+    }
+
 else:
     # AWS Setting
     AWS_REGION = Secret.AWS_REGION
@@ -52,47 +97,5 @@ INSTALLED_APPS += [
     'storages',
 ]
 
-
-# logging
-LOGGING = {
-    'version': 1,
-    # 기존의 로깅 설정을 비활성화 할 것인가?
-    'disable_existing_loggers': False,
-
-    # 포맷터
-    # 로그 레코드는 최종적으로 텍스트로 표현됨
-    # 이 텍스트의 포맷 형식 정의
-    # 여러 포맷 정의 가능
-    'formatters': {
-        'format1': {
-            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
-            'datefmt': '%d/%b/%Y %H:%M:%S'
-        },
-        'format2': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-
-    # 핸들러
-    # 로그 레코드로 무슨 작업을 할 것인지 정의
-    # 여러 핸들러 정의 가능
-    'handlers': {
-        # 콘솔(터미널)에 출력
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'format2',
-        }
-    },
-
-    # 로거
-    # 로그 레코드 저장소
-    # 로거를 이름별로 정의
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-
-}
+SESSION_COOKIE_AGE = 609600
+SESSION_SAVE_EVERY_REQUEST = True
