86d67188c640b40067f526c3d492560e400d7fd1
younmin Yoon <hihihixxx89@gmail.com>
2019-12-03 17:22:45 +0900

    - connect aws

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -1,7 +1,12 @@
 import os
+from conf.settings.secret import Secret
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
+module = None
+if not Secret:
+    module = '.local'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings'+module)
 
 application = get_wsgi_application()
