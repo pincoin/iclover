a6c337be38338ff572812a69f2c1e474cd28edd3
younmin Yoon <hihihixxx89@gmail.com>
2019-12-05 16:18:23 +0900

    - connect aws

--- conf/settings/storage.py
+++ conf/settings/storage.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
+    default_acl = 'private'
     location = settings.MEDIAFILES_LOCATION
 
 class StaticStorage(S3Boto3Storage):
+    default_acl = 'public-read'
     location = settings.STATICFILES_LOCATION
